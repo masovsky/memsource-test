8f2aa5dfbef16c7a97bfdc5cf4bd0555b600016c
Jaroslav Masovsky <jaroslav.masovsky@deutsche-boerse.com>
2022-03-02 17:49:04 +0100

    Init project with springboot and react page.

---
+++ frontend/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Running...', () => {
+  render(<App />);
+  const runningElement = screen.getByText(/Running.../i);
+  expect(runningElement).toBeDefined();
+});
