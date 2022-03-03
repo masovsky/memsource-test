92addc4b7387ffbbaa71d8aa982234dd76532786
Jaroslav Masovsky <jaroslav.masovsky@deutsche-boerse.com>
2022-03-03 13:45:24 +0100

    Added logo.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Notifications from './notifications/Notifications';
+import { NavLink } from 'react-router-dom';
+import logo from '../assets/logo-light.svg'
 
 function App() {
     return (
         <div>
             <Notifications />
+            <NavLink to="/">
+                <img src={logo} alt="MEMSOURCE" />
+            </NavLink>
         </div>
     )
 }
