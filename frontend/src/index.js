c386757ccab5ab39a358df70a4dae7546b36bfeb
Jaroslav Masovsky <jaroslav.masovsky@deutsche-boerse.com>
2022-03-03 08:37:49 +0100

    Moved tests to __tests__ folder and css to _styles_ folder.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './__styles__/index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
